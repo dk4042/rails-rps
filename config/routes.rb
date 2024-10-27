0d2221b310b816bcadb3bae73c315116e91a1df6
dk4042 <dkhamaza@uchicago.edu>
2024-10-27 17:13:45 +0000

    dd

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  get("/rock", { :controller =>"zebra", :action => "giraffe"})
+  get("/rock", { :controller => "zebra", :action => "giraffe"})
+
+  get("/paper", { :controller => "zebra", :action => "elephant"})
+
+  get("/scissors", { :controller => "zebra", :action => "lion"})
+
 end
