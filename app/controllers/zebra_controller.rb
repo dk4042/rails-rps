9ba6965574cbf82e9580564e8957769779213520
dk4042 <dkhamaza@uchicago.edu>
2024-10-27 17:14:39 +0000

    c

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -1,7 +1,7 @@
 class ZebraController < ApplicationController
   def giraffe
     @random_move = ["rock", "paper", "scissors"].sample
-     render({ :template => "game_templates/play_rock"})
+  
      if @random_move == "rock"
       @outcome = "tied"
     elsif @random_move == "paper"
@@ -9,5 +9,6 @@ class ZebraController < ApplicationController
     else 
       @outcome = "won"
     end
+    render({ :template => "game_templates/play_rock"})
   end
 end 
